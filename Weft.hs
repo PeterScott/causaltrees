746656ca07af641437985b8cb9f3495108e67764
Peter Scott <pjscott@iastate.edu>
2010-11-18 17:31:55 -0600

    Changed Weft API to be more tuple-based, which I find pleasant. Added listToWeft function.

--- Weft.hs
+++ Weft.hs
@@ -41,14 +41,18 @@ class Weft a where
     --   specify. Note that if the value is smaller than the current
     --   offset, the current offset will be replaced. If you don't
     --   want this behavior, use extendWeft.
-    setWeft :: a -> Char -> Word32 -> a
+    setWeft :: a -> (Char, Word32) -> a
     -- | Set the offset of the given yarn to the value you specify or
     --   the existing value, whichever is larger.
-    extendWeft :: a -> Char -> Word32 -> a
-    extendWeft w yarn offset = setWeft w yarn offset'
+    extendWeft :: a -> (Char, Word32) -> a
+    extendWeft w (yarn, offset) = setWeft w (yarn, offset')
         where offset' = max offset (getWeft w yarn)
     -- | Return an ordered list of (yarn, offset) pairs.
     weftToList :: a -> [(Char, Word32)]
+    -- | Convert an unordered list of (yarn, offset) pairs into a
+    --   Weft.
+    listToWeft :: [(Char, Word32)] -> a
+    listToWeft = foldl' setWeft emptyWeft
 
 -- | A map-based implementation of a 'Weft'. This may share memory
 --   with other, closely related wefts, and has O(lg n) time
@@ -57,12 +61,12 @@ newtype WeftMap = WeftMap (IntMap.IntMap Word32)
     deriving (Eq, Show)
 
 instance Weft WeftMap where
-    emptyWeft                       = WeftMap IntMap.empty
-    getWeft (WeftMap m) yarn        = case IntMap.lookup (fromIntegral $ ord yarn) m of
-                                        Just offset -> offset
-                                        Nothing     -> 0
-    setWeft (WeftMap m) yarn offset = WeftMap $ IntMap.insert (fromIntegral $ ord yarn) offset m
-    weftToList (WeftMap m)          = map (\(y, o) -> (chr y, o)) $ IntMap.assocs m
+    emptyWeft                          = WeftMap IntMap.empty
+    getWeft (WeftMap m) yarn           = case IntMap.lookup (fromIntegral $ ord yarn) m of
+                                           Just offset -> offset
+                                           Nothing     -> 0
+    setWeft (WeftMap m) (yarn, offset) = WeftMap $ IntMap.insert (fromIntegral $ ord yarn) offset m
+    weftToList (WeftMap m)             = map (\(y, o) -> (chr y, o)) $ IntMap.assocs m
 
 ----------------------------------------------------------------------------------------------
 
@@ -82,9 +86,9 @@ instance Weft WeftUArray where
                                     Right i -> a ! (2*i + 1)
                                     Left  _ -> 0
         where yarnNum = fromIntegral $ ord yarn
-    setWeft (WeftUArray a) yarn offset = case binSearch a yarnNum $ bounds' a of 
-                                           Right i -> WeftUArray $ a // [(2*i + 1, offset)]
-                                           Left  j -> WeftUArray $ insArray a (yarnNum, offset) j
+    setWeft (WeftUArray a) (yarn, offset) = case binSearch a yarnNum $ bounds' a of 
+                                              Right i -> WeftUArray $ a // [(2*i + 1, offset)]
+                                              Left  j -> WeftUArray $ insArray a (yarnNum, offset) j
         where yarnNum = fromIntegral $ ord yarn
     weftToList (WeftUArray a) = chunkifyWeftArray $ elems a
         where chunkifyWeftArray (a : (b : tl)) = (chr $ fromIntegral a, b) : (chunkifyWeftArray tl)
@@ -124,9 +128,8 @@ insArray a (yarnNum, offset) idx = foo
 --   efficient, as it may reallocate a lot of arrays. The input weft2
 --   need not be in sorted order.
 weft2ToWeft :: (Weft a) => L.Text -> a
-weft2ToWeft weft2 = foldl' setWeftTuple emptyWeft $ map tuplify $ L.chunksOf 2 weft2
+weft2ToWeft weft2 = listToWeft $ map tuplify $ L.chunksOf 2 weft2
     where tuplify txt = (L.head txt, fromIntegral $ (ord $ L.last txt) - (ord '0'))
-          setWeftTuple weft (yarn, offset) = setWeft weft yarn offset
 
 -- | Convert a 'Weft' into a weft2, such as "a5b3d1". The resulting
 --   weft2 will be in sorted order.
@@ -135,10 +138,9 @@ weftToWeft2 = TLB.toLazyText . (foldl' mappend mempty) . (map toChunk) . weftToL
     where toChunk (yarn, offset) =
               (TLB.singleton yarn) `mappend` (TLB.singleton $ chr $ ((fromIntegral offset) + (ord '0')))
 
+-- For debugging, some example data:
 -- w1 = (weft2ToWeft $ L.pack "a5b3d1") :: WeftUArray
--- (WeftUArray w1arr) = w1
-
--- w2 :: WeftMap
--- w2_a5 = setWeft emptyWeft 'a' 5
--- w2_a5b3 = setWeft w2_a5 'b' 3
--- w2 = setWeft w2_a5b3 'd' 1
+-- w2     :: WeftMap
+-- w2_a5   = setWeft emptyWeft ('a', 5)
+-- w2_a5b3 = setWeft w2_a5     ('b', 3)
+-- w2      = setWeft w2_a5b3   ('d', 1)

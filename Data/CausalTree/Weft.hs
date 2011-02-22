314dbb241be01d6243a8d9f30851d03858570c33
Peter Scott <pjscott@iastate.edu>
2011-02-22 17:02:23 -0600

    Create Weft class, and two instances.
    One instance uses hashmaps, and has data sharing and guaranteed O(lg n) time on
    all operations, with better speed than regular Map. The other instance uses
    unboxed vectors, and is optimized for fast lookups.

---
+++ Data/CausalTree/Weft.hs
@@ -0,0 +1,108 @@
+module Data.CausalTree.Weft (
+              Weft (emptyWeft
+                   , getWeft
+                   , setWeft
+                   , extendWeft
+                   , weftToList)
+
+            , WeftMap
+            , WeftVec
+            ) where
+
+import Data.CausalTree.Atom (Yarn, Offset)
+import qualified Data.Vector.Unboxed as V
+import Data.Vector.Unboxed (Vector, (!?), (!))
+import qualified Data.HashMap.Strict as M
+import Data.List (foldl')
+
+--------------------------
+-- High-level abstractions
+--------------------------
+
+-- | A 'Weft' is a mapping of yarns to maximum offsets. It can be implemented in
+-- multiple ways, but all of these implementations use this same type class.
+class Weft a where
+    -- | Return an empty 'Weft'
+    emptyWeft :: a
+    -- | Get the top of a given yarn in a 'Weft', or 0 if the yarn is not found.
+    getWeft :: a -> Yarn -> Offset
+    -- | Set the offset of the given yarn to the value you specify. Note that if
+    -- the value is smaller than the current offset, the current offset will be
+    -- replaced. If you don't want this behavior, use extendWeft.
+    setWeft :: a -> (Yarn, Offset) -> a
+    -- | Set the offset of the given yarn to the value you specify or the
+    -- existing value, whichever is larger.
+    extendWeft :: a -> (Yarn, Offset) -> a
+    extendWeft w (yarn, offset) = setWeft w (yarn, offset')
+        where offset' = max offset (getWeft w yarn)
+    -- | Return an ordered list of (yarn, offset) pairs.
+    weftToList :: a -> [(Yarn, Offset)]
+    -- | Convert an unordered list of (yarn, offset) pairs into a Weft.
+    listToWeft :: [(Yarn, Offset)] -> a
+    listToWeft = foldl' setWeft emptyWeft
+
+
+-----------------
+-- Map-based weft
+-----------------
+
+-- | A hashmap-based implementation of a 'Weft'. This may share memory with
+-- other, closely related wefts, and has O(lg n) time complexity for most
+-- everything.
+newtype WeftMap = WeftMap (M.HashMap Yarn Offset)
+    deriving (Eq, Show)
+
+instance Weft WeftMap where
+    emptyWeft                          = WeftMap M.empty
+    getWeft (WeftMap m) yarn           = case M.lookup yarn m of
+                                           Just offset -> offset
+                                           Nothing     -> 0
+    setWeft (WeftMap m) (yarn, offset) = WeftMap $ M.insert yarn offset m
+    weftToList (WeftMap m)             = M.toList m
+
+
+--------------------
+-- Vector-based weft
+--------------------
+
+newtype WeftVec = WeftVec (Vector (Yarn, Offset))
+    deriving (Eq, Show)
+
+instance Weft WeftVec where
+    emptyWeft = WeftVec V.empty
+    getWeft (WeftVec vec) yarn =
+        case bisectRight vec yarn of
+          0 -> 0
+          i -> let (y, offset) = vec ! (i-1)
+               in if y == yarn then offset else 0
+    setWeft (WeftVec vec) (yarn, offset) =
+        case bisectRight vec yarn of
+          0 -> WeftVec $ V.cons (yarn, offset) vec
+          i -> let (y, o) = vec ! (i-1)
+               in if y == yarn then
+                      WeftVec $ vec `V.unsafeUpd` [((i-1), (yarn, offset))]
+                  else
+                      WeftVec $ V.concat [ V.unsafeTake i vec
+                                         , V.singleton (yarn, offset)
+                                         , V.unsafeDrop i vec ]
+    weftToList (WeftVec vec) = V.toList vec
+
+
+----------------
+-- Binary search
+----------------
+
+-- | Locate the insertion point for a 'Yarn' in an ordered @(Yarn, Offset)@
+-- vector, which lies to the right of any entry with an identical yarn. Requires
+-- that there be no two entries in the vector with the same yarn.
+bisectRight :: Vector (Yarn, Offset) -> Yarn -> Int
+bisectRight vec yarn = bs 0 (V.length vec)
+    where bs l r | l > r = l
+                 | otherwise = case vec !? m of
+                                 Nothing  -> V.length vec
+                                 Just mid ->
+                                     case compare (fst mid) yarn of
+                                       GT -> bs l (m-1)
+                                       LT -> bs (m+1) r
+                                       EQ -> m+1
+                     where m = l + ((r - l) `div` 2)

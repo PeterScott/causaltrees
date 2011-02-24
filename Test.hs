fa44de94014d3a4e10a3242a2e754d978308ffac
Peter Scott <pjscott@iastate.edu>
2011-02-24 15:21:38 -0600

    Clean up and expand QuickCheck tests.

--- Test.hs
+++ Test.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TemplateHaskell #-}
 import Data.CausalTree.Atom
 import Data.CausalTree.Weft
 import Data.CausalTree.SerDes
@@ -7,6 +8,7 @@ import Data.Binary.Put
 import qualified Data.Vector.Storable as SV
 
 import Test.QuickCheck
+import Test.QuickCheck.All
 
 ------------------------
 -- QuickCheck properties
@@ -17,23 +19,18 @@ prop_compressed_id n = runGet getC32 (runPut $ putC32 n) == n
 prop_compressed_idP a b = runGet getP32 (runPut $ putP32 (a,b)) == (a,b)
 
 -- Atom.hs
-prop_encdec id pred c = decode (encode atom) == atom
+prop_encdec_atom id pred c = decode (encode atom) == atom
     where atom = TextAtom id pred c
 
 prop_storable id pred c = SV.head vec == atom
     where atom = TextAtom id pred c
           vec  = SV.singleton atom
 
--- Top level
-runQC = sequence_ [ quickCheck prop_compressed_id
-                  , quickCheck prop_compressed_idP
-                  , quickCheck prop_encdec
-                  , quickCheck prop_storable
-                  ]
+-- Weft.hs
+prop_encdec_weftvec a b c d = decode (encode vv) == vv
+    where vv = orderedListToWeft [(a, a+1), (b, b+1), (c, c+1), (d, d+1)] :: WeftVec
 
-main = do
-  putStrLn "Running QuickCheck tests..."
-  runQC
-  putStrLn "Testing serialization"
-  let vv = orderedListToWeft [(1, 11), (3, 33), (4, 44), (10, 1000)] :: WeftVec
-  print $ encode vv
+-- Top level
+runQC = $(quickCheckAll)
+
+main = runQC

c59f1357e21a5530d923767fa5c199893eb0df46
Peter Scott <pjscott@iastate.edu>
2011-02-24 19:34:56 -0600

    Create preliminary cursor implementation.

--- Data/CausalTree/Weave.hs
+++ Data/CausalTree/Weave.hs
@@ -1,21 +1,27 @@
-{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE BangPatterns, ExistentialQuantification, RankNTypes #-}
 module Data.CausalTree.Weave where
 
 import Data.CausalTree.Atom
 import Data.CausalTree.Weft
 import qualified Data.HashMap.Strict as M
 import qualified Data.Vector as BV
+import qualified Data.Vector.Storable.Mutable as MV
 import qualified Data.Vector.Storable as V
 import Data.Vector.Storable (Vector, (!?), (!))
 import Foreign.Storable
 import Data.Binary
 import Data.List (nub, null, foldl')
+import qualified Control.Monad.State as State
+import Control.Monad
+import Data.STRef
 
 import qualified Data.ByteString.Lazy as L
 import Data.ByteString.Lazy (ByteString)
 import qualified Blaze.ByteString.Builder as BB
 import qualified Blaze.ByteString.Builder.Char.Utf8 as BB
 import Data.Monoid
+
+import System.IO.Unsafe
 
 --------------------------
 -- High-level abstractions
@@ -182,3 +188,178 @@ cw_test = emptyWeave { vwVec = V.fromList v
               , taEnd
               ]
           memoDict = addToMemoDict emptyMemoDict (1,1) (orderedListToWeft [(0, 2)])
+
+
+------------------------
+-- Weave transformations
+------------------------
+
+-- FIXME: add a proper class and stuff.
+
+-- Things stored in the state:
+-- * Current position in array :: Int
+-- * List of non-sticky insertion chains :: [(Int, Vector TextAtom)]
+-- * List of deletions :: [(Int, TextAtom)]
+-- * List of save-awareness atoms :: [TextAtom]
+-- All lists are backwards, so when copying, work backwards. :-|
+
+data VwTransS = VwTransS { vwtIdx :: Int -- ^ Current index
+                         , vwtWeave :: VectorWeave -- ^ Weave
+                         , vwtInsChains :: [(Int, Vector TextAtom)] -- ^ non-sticky ins chains
+                         , vwtDeletors :: [(Int, TextAtom)] -- ^ deletors and pred positions
+                         , vwtDeletions :: [Int]            -- ^ indices to delete
+                         , vwtSaveAwareness :: [TextAtom] -- ^ save-awareness atoms
+                         } deriving Show
+type VwTrans a = State.State VwTransS a
+
+vwtInitialState weave = VwTransS { vwtIdx = 0, vwtWeave = weave
+                                 , vwtInsChains = [], vwtDeletors = []
+                                 , vwtDeletions = [], vwtSaveAwareness = []}
+
+-- | Go to next cursor position. Will not go past weave length.
+cursorNext :: VwTrans ()
+cursorNext = State.modify trans
+    where trans s = let i = min (vwtIdx s + 1) (maxIdx s) in s { vwtIdx = i }
+          maxIdx = V.length . vwVec . vwtWeave
+
+-- | Get the Atom at the current position in the cursor, if any.
+cursorCurrent :: VwTrans (Maybe TextAtom)
+cursorCurrent = State.get >>= \s -> return $ (vwVec (vwtWeave s)) !? (vwtIdx s)
+
+-- | Get the Atom after the current position in the cursor, if any.
+cursorPeek :: VwTrans (Maybe TextAtom)
+cursorPeek = State.get >>= \s -> return $ (vwVec (vwtWeave s)) !? (vwtIdx s + 1)
+
+-- | Is the cursor at the beginning of the weave?
+cursorAtBeginning :: VwTrans Bool
+cursorAtBeginning = State.get >>= (return . (==0) . vwtIdx)
+
+-- | Is the cursor at the end of the weave?
+cursorAtEnd :: VwTrans Bool
+cursorAtEnd = do s <- State.get
+                 let maxIdx = V.length (vwVec (vwtWeave s))
+                 return $ vwtIdx s == maxIdx
+
+-- | Insert a non-sticky chain at the current location. Updates awareness weft
+-- and memoization dict.
+cursorInsert :: Vector TextAtom -> VwTrans ()
+cursorInsert chain = State.modify $ \s -> 
+    s { vwtInsChains = (vwtIdx s, chain) : (vwtInsChains s) }
+
+-- | Insert a deletion atom referring to the current location. Updates awareness
+-- weft and memoization dict.
+cursorDeleteWith :: TextAtom -> VwTrans ()
+cursorDeleteWith delatom = State.modify $ \s -> 
+    s { vwtDeletors = (vwtIdx s, delatom) : (vwtDeletors s) }
+
+-- | Delete the current location.
+cursorDelete :: VwTrans ()
+cursorDelete = State.modify $ \s -> 
+    s { vwtDeletions = (vwtIdx s) : (vwtDeletions s) }
+
+-- | Tack a list of save-awareness atoms onto the weave. Updates awareness weft
+-- and memoization dict.
+cursorSaveAwareness :: [TextAtom] -> VwTrans ()
+cursorSaveAwareness saAtoms = State.modify $ \s -> 
+    s { vwtSaveAwareness = saAtoms ++ (vwtSaveAwareness s) }
+
+-- Utility function: generate list [top..bottom], where top > bottom.
+downTo :: Int -> Int -> [Int]
+top `downTo` bottom | top == bottom = [top]
+                    | otherwise = top : ((top-1) `downTo` bottom)
+
+-- | Execute a cursor transformation on a weave.
+
+-- FIXME: make this handle also the updating of memo dict and top weft. Once
+-- vector is computed, use patchBlockers to find which waiting patches can now
+-- be applied, and return those as well. Uncomment type decl.
+
+--execCursor :: VectorWeave -> VwTrans a -> (VectorWeave, [Patch])
+execCursor w trans = V.create createNewVector -- FIXME
+    where t@(VwTransS idx _ insChains deletors deletions saAtoms) = State.execState trans initState
+          initState = vwtInitialState w
+          vw = vwVec w
+          pop ref = modifySTRef ref tail
+          -- Calculate size of resulting vector
+          resultSize = (V.length vw) + (sum $ map (V.length . snd) insChains)
+                       + (length deletors) + (length saAtoms) - (length deletions)
+          -- Copy over all save-awareness atoms, and decrement j by that many.
+          copy_awareness vec j =
+            forM_ saAtoms $ \atom ->
+                do jj <- readSTRef j
+                   MV.write vec jj atom
+                   writeSTRef j (jj-1)
+          -- Process a deletor: copy it over, then copy the atom
+          processDeletor vec i j deletor = do
+            jj <- readSTRef j
+            MV.write vec jj deletor
+            MV.write vec (jj - 1) (vw ! i)
+            writeSTRef j (jj - 2)
+          -- Copy ins chain, then atom
+          processInsChain vec i j chain = do
+            forM_ ((V.length chain - 1) `downTo` 0) $ \k -> 
+                do jj <- readSTRef j
+                   MV.write vec jj (chain ! k)
+                   writeSTRef j (jj-1)
+            jj <- readSTRef j
+            MV.write vec jj (vw ! i)
+            writeSTRef j (jj - 1)
+          -- Do the steps above, and return an initialized mutable vector
+          createNewVector = do
+            -- Allocate a mutable vector of the correct size
+            vec <- MV.new resultSize
+            j <- newSTRef $ resultSize - 1
+            insChains_ <- newSTRef insChains
+            deletors_ <- newSTRef deletors
+            deletions_ <- newSTRef deletions
+            copy_awareness vec j
+            forM_ ((V.length vw - 1) `downTo` 0) $ \i ->
+                do deletors'  <- readSTRef deletors_
+                   insChains' <- readSTRef insChains_
+                   deletions' <- readSTRef deletions_
+                   (if deletors' /= [] && fst (head deletors') == i then
+                        processDeletor vec i j (snd (head deletors')) >> pop deletors_
+                    else
+                        if insChains' /= [] && fst (head insChains') == i then
+                            processInsChain vec i j (snd (head insChains')) >> pop insChains_
+                        else 
+                            if deletions' /= [] && head deletions' == i then
+                                pop deletions_
+                            else do
+                              jj <- readSTRef j
+                              MV.write vec jj (vw ! i)
+                              writeSTRef j (jj-1))
+            return vec
+          
+          
+
+-- How to execute a cursor:
+--
+-- First, calculate the size of the resulting vector, in atoms. This will be:
+-- Original size, PLUS sum of lengths of insertion chains, PLUS length of
+-- deletors, PLUS length of save-awareness list, MINUS length of deletions
+-- list. Allocate a mutable vector this size.
+--
+-- Next, starting at j = (len vec) - 1 and i = (len weave-vec) - 1 and going
+-- backward down to 0 on both, we decide what to do for each i. First, copy over
+-- all the save-awareness atoms, and decrement j by that many.
+
+-- NEXT: If we see a deletion here, just i--. If we see a deletor here, copy it
+-- to vec[j], copy wv[i] to vec[j-1], and j -= 2, i--. If we see an ins-chain
+-- here, copy it in BACKWARDS, then copy over wv[i] to vec[j-len(chain)], and
+-- decrement j by len(chain)+1, i--. Go until i < 0, then stop.
+
+curPrint :: Show a => a -> VwTrans ()
+curPrint x = unsafePerformIO (print x) `seq` return ()
+
+-- Transform cw_test into cw
+cwt_trans = do
+  cursorNext
+--  cursorDelete >> cursorNext -- Delete the 'T'
+  cursorNext      -- cursor after 'e'
+--  cursorCurrent >>= curPrint    -- print the 'e' atom
+  cursorInsert $ V.singleton (taChar (2,2) (1,2) 'x')
+  cursorNext                    -- cursor after 's'
+--  cursorCurrent >>= curPrint    -- print the 'e' atom
+  cursorDeleteWith $ taDeletor (2,1) (1,3) -- Delete 's'
+  cursorSaveAwareness [taSaveAwareness (1,5) (2,2)]
